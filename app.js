65a6a4a6c90056ac7d5c07b2c5e6194e82b6cc1c
khalil <khalillakhdharatc@gmail.com>
2022-06-16 09:57:28 +0100

    reading

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
+
+
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyC01IEfpu0lzVuokygFN43ysXUb5a5NUsw",
-    authDomain: "demofire-e797c.firebaseapp.com",
-    databaseURL: "https://demofire-e797c-default-rtdb.firebaseio.com",
-    projectId: "demofire-e797c",
-    storageBucket: "demofire-e797c.appspot.com",
-    messagingSenderId: "786126058816",
-    appId: "1:786126058816:web:e7e39f9b90fb05db0436a5",
-    measurementId: "G-L5Z86FTT0Z"
-    };
-    firebase.initializeApp(config)
+	apiKey: "AIzaSyCnZuvAQNU5PSeD4OLfwIRh6O1JfYQpz2c",
+	authDomain: "firejs-a52f7.firebaseapp.com",
+    databaseURL: "https://firejs-a52f7-default-rtdb.firebaseio.com/",
+	projectId: "firejs-a52f7",
+    storageBucket: "firejs-a52f7.appspot.com",
+    messagingSenderId: "784973263653",
+    appId: "1:784973263653:web:d0f865dfd273507a77a0cc",
+    measurementId: "G-C8N6BCC5L6"
+};
+
+
+
+firebase.initializeApp(config);
+
+// Firebase Database Reference and the child
 const dbRef = firebase.database().ref();
-const userRef= dbRef.child('users');
-function readUserData()
-{
-    
+const usersRef = dbRef.child('users');
+
+
+readUserData(); 
+	
+
+// --------------------------
+// READ
+// --------------------------
+function readUserData() {
+
+	const userListUI = document.getElementById("user-list");
+
+	usersRef.on("value", snap => {
+
+		userListUI.innerHTML = ""
+
+		snap.forEach(childSnap => {
+
+			let key = childSnap.key,
+				value = childSnap.val()
+  			
+			let $li = document.createElement("li");
+
+			$li.innerHTML = value.name;
+
+			$li.setAttribute("user-key", key);
+			//$li.addEventListener("click", userClicked)
+			userListUI.append($li);
+
+ 		});
+
+
+	})
+
 }
